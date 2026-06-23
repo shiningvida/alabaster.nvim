d810180a4f220bed0713b15075b4c2ba260d51ee
shiningvida <289472511+shiningvida@users.noreply.github.com>
2026-06-22 18:12:38 -0700

    Update alabaster.lua

--- lua/lualine/themes/alabaster.lua
+++ lua/lualine/themes/alabaster.lua
@@ -1,63 +1,45 @@
 local colors
 if vim.o.background == "dark" then
-	colors = {
-		bg       = "#162022",
-		bg_dark  = "#0e1415",
-		bg_light = "#1d2d30",
-		fg       = "#cecece",
-		muted    = "#708b8d",
-		subtle   = "#4a6568",
-		normal   = "#71ade7",
-		insert   = "#95cb82",
-		visual   = "#cc8bc9",
-		replace  = "#cd974b",
-		command  = "#47bea9",
-	}
+    colors = {
+        color1 = "#162022",
+        color2 = "#cd974b",
+        color3 = "#cecece",
+        color4 = "#9F9F9F",
+        color5 = "#333333",
+        color6 = "#cc8bc9",
+    }
 else
-	colors = {
-		bg       = "#c9c9c9",
-		bg_dark  = "#b0b0b0",
-		bg_light = "#dedede",
-		fg       = "#222222",
-		muted    = "#666666",
-		subtle   = "#999999",
-		normal   = "#325cc0",
-		insert   = "#448c27",
-		visual   = "#7a3e9d",
-		replace  = "#cb9000",
-		command  = "#0083b2",
-	}
+    colors = {
+        color1 = "#c9c9c9",
+        color2 = "#cb9000",
+        color3 = "#222222",
+        color4 = "#666666",
+        color5 = "#aaaaaa",
+        color6 = "#7a3e9d",
+    }
 end
 
 return {
-	normal = {
-		a = { fg = colors.bg_dark, bg = colors.normal, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg_light },
-		c = { fg = colors.muted, bg = colors.bg },
-	},
-	insert = {
-		a = { fg = colors.bg_dark, bg = colors.insert, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg_light },
-		c = { fg = colors.muted, bg = colors.bg },
-	},
-	visual = {
-		a = { fg = colors.bg_dark, bg = colors.visual, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg_light },
-		c = { fg = colors.muted, bg = colors.bg },
-	},
-	replace = {
-		a = { fg = colors.bg_dark, bg = colors.replace, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg_light },
-		c = { fg = colors.muted, bg = colors.bg },
-	},
-	command = {
-		a = { fg = colors.bg_dark, bg = colors.command, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.bg_light },
-		c = { fg = colors.muted, bg = colors.bg },
-	},
-	inactive = {
-		a = { fg = colors.subtle, bg = colors.bg },
-		b = { fg = colors.subtle, bg = colors.bg },
-		c = { fg = colors.subtle, bg = colors.bg },
-	},
+    replace = {
+        a = { fg = colors.color1, bg = colors.color2, gui = "bold" },
+        b = { fg = colors.color3, bg = colors.color1 },
+    },
+    inactive = {
+        a = { fg = colors.color4, bg = colors.color1, gui = "bold" },
+        b = { fg = colors.color4, bg = colors.color1 },
+        c = { fg = colors.color4, bg = colors.color1 },
+    },
+    normal = {
+        a = { fg = colors.color3, bg = colors.color5, gui = "bold" },
+        b = { fg = colors.color3, bg = colors.color1 },
+        c = { fg = colors.color3, bg = colors.color1 },
+    },
+    visual = {
+        a = { fg = colors.color1, bg = colors.color6, gui = "bold" },
+        b = { fg = colors.color3, bg = colors.color1 },
+    },
+    insert = {
+        a = { fg = colors.color1, bg = colors.color3, gui = "bold" },
+        b = { fg = colors.color3, bg = colors.color1 },
+    },
 }
